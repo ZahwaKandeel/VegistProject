479c680420556ed90428476af897a5b1460ef08d
ZahwaKandeel <zahwa.bm.kandeel@gmail.com>
2026-02-18 13:37:55 +0200

    working in js

--- productDetials/JS/ProductDetails.js
+++ productDetials/JS/ProductDetails.js
@@ -16,14 +16,34 @@
 
 
 // when buy it now button clicked it goes to check out page
+// document.addEventListener("DOMContentLoaded", function () {
+
+//     const buyItNowBtn = document.querySelector(".btn-dark");
+
+//     if (buyItNowBtn) {
+//         buyItNowBtn.addEventListener("click", function () {
+//             window.location.href = "VegistProject/checkOut/Template/checkOut.html";
+//         });
+//     }
+
+// });
+
+
+// --- Event Listener 1: Add to Cart ---
 document.addEventListener("DOMContentLoaded", function () {
+    const addToCartButtons = document.querySelectorAll(".btn-warning");
 
-    const buyItNowBtn = document.querySelector(".btn-dark");
-
-    if (buyItNowBtn) {
-        buyItNowBtn.addEventListener("click", function () {
-            window.location.href = "VegistProject/checkOut/Template/checkOut.html";
-        });
-    }
-
+    addToCartButtons.forEach(function (button) {
+        // We check the text to make sure we hit the right button
+        if (button.textContent.trim().toLowerCase() === "add to cart") {
+            button.addEventListener("click", function() {
+                redirectToCart();
+            });
+        }
+    });
 });
+
+function redirectToCart() {
+    console.log("Navigating to Cart...");
+    window.location.href = "cart/Template/cart.html";
+}
