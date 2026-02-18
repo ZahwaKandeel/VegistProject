c3c7e0bb208571f7679e9f9bd74e2336bb5d8f98
YamenAly <yamen.halim@gmail.com>
2026-02-18 23:19:11 +0200

    product.js added

---
+++ componant/Product.js
@@ -0,0 +1,56 @@
+export class Product{
+    constructor(id, name, price, description, stock, category, imageUrl){
+        this._id =id;
+        this._name = name;
+        this._price = price;
+        this._description = description;
+        this._stock = stock;
+        this._category = category;
+        this._imageUrl = imageUrl;
+    }
+
+    set ID(_id)
+    {
+        this.id = _id;
+    }
+    get ID()
+    {
+        return this.id
+    }
+    
+    set Name(_name)
+    {
+        this.name = _name;
+    }
+    get Name()
+    {
+        return this.name;
+    }
+
+    set Price(_price)
+    {
+        this.price = _price;
+    }
+    get Price()
+    {
+        return this.price;
+    }
+    
+    set Description(_description)
+    {
+        this.description = _description;
+    }
+    get Description()
+    {
+        return this.description;
+    }
+
+    set Stock(_stock)
+    {
+        this.stock = _stock;
+    }
+    get Stock()
+    {
+        return this.stock;
+    }
+}
