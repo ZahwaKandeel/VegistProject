4c4b479012b9a9e657ab4f0d7623e204c8f9b402
ZahwaKandeel <zahwa.bm.kandeel@gmail.com>
2026-02-18 11:18:03 +0200

    test deployment

---
+++ productDetials/JS/ProductDetails.js
@@ -0,0 +1,29 @@
+// when add to cart button clicked it goes to add to cart page
+document.addEventListener("DOMContentLoaded", function () {
+
+    const warningButtons = document.querySelectorAll(".btn-warning");
+
+    warningButtons.forEach(function (button) {
+
+        if (button.textContent.trim() === "Add to cart") {
+            button.addEventListener("click", function () {
+                window.location.href = "cart/Template/cart.html";
+            });
+        }
+    });
+});
+
+
+
+// when buy it now button clicked it goes to check out page
+// document.addEventListener("DOMContentLoaded", function () {
+
+//     const buyItNowBtn = document.querySelector(".btn-dark");
+
+//     if (buyItNowBtn) {
+//         buyItNowBtn.addEventListener("click", function () {
+//             window.location.href = "checkOut.html";
+//         });
+//     }
+
+// });
