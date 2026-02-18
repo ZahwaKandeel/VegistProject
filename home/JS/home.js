6af3fc93833c7baba5aacda99e55bc93172a8d89
YamenAly <yamen.halim@gmail.com>
2026-02-18 13:49:33 +0200

    shop now button fixed

--- home/JS/home.js
+++ home/JS/home.js
@@ -1,5 +1,5 @@
 $(function(){
-$("#shopnow").click(function(){
-    window.location.href = "/productList/Template/product_list.html"
-});
+    $(".shopnow").click(function(){
+        window.location.href = "/productList/Template/product_list.html"
+    });
 });
