05113b8de370e5928c79b47ff904ff6ffd7c571c
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2026-02-18 18:40:58 +0200

    added order, product, user prototypes in models folder

---
+++ models/product.js
@@ -0,0 +1,50 @@
+// Product prototype
+
+const resetDB = () => {
+    const Product = [
+        {
+            id: 0,
+            title: "",
+            rating: {
+                total: 10,
+                count: 2,
+            },
+            stock: 0,
+            description: "",
+            size: [""],
+            matiral: [""],
+            qty: 0,
+            serial: "",
+            details: "Rich text",
+            images: ["path"],
+            price: 0,
+            sale: 0,
+            discountvalue: 0,
+            discountPercentage: 0, // %
+        },
+    ];
+    localStorage.setItem("Product", JSON.stringify(Product));
+};
+// const product = {
+//     form.title,
+//     form.description,
+//     form.stock
+//     rating:{
+//         total:0,
+//         count:0
+//     }
+// }
+
+
+// GET products
+const getDB=()=>{
+    const products = localStorage.getItem("Product")
+    return JSON.parse(products)
+}
+
+// SET products
+const addProduct = (product)=>{
+    const products = getDB()
+    products.push(product)
+    localStorage.setItem("Product",JSON.stringify(products))
+}
