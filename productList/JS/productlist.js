952560f12c2582ec2efdd4227d5a1404b23f191a
Sami <ahmedsamy121277@gmail.com>
2026-02-18 13:28:10 +0200

    fisrt js

--- productList/JS/productlist.js
+++ productList/JS/productlist.js
@@ -60,9 +60,66 @@ console.log(response.products[1].thumbnail)
                         </div>
                     </div>
                 `;
+                let html2 = `
+                  <div class="cards  col-12  my-5   " "> <!--div of card 1  with icons   -->
+
+<div     class="     row  "  >
+                    <div class=" position-relative col-12 col-lg-4  "  style="height: 250px;" >    <!--Image -->
+    <img src="${product.thumbnail}" class=" w-100 main-img   object-fit-cover h-100"> 
+    <img src="../images//1.2.jpg" class=" w-100  hover-img position-absolute top-0 start-0 h-100  ">
+</div>
+
+<div class="  col-12 col-lg-8 ">     <!--   name and price and rationg  -->
+
+
+<p class=""> <a href=""  class="para text-decoration-none  " >${product.title}</a></p>  <!--   name and price   -->
+        <p class="fw-bold">${product.price}</p>   
+
+        <div class="d-flex align-items-center  "> <!-- review by stars -->
+    <div class="text-warning me-2 rating ">
+      <i class=" fa-regular fa-star"></i>
+      <i class="fa-regular fa-star"></i>
+      <i class="fa-regular fa-star"></i>
+      <i class="fa-regular fa-star"></i>
+     <i class="fa-regular fa-star"></i>
+      
+    </div>
+    <span class="text-muted ratingspa">No reviews</span>
+
+  </div>    <!--   name and price   -->
+
+
+
+  <div> <!--   paragraph   -->
+
+            <p class="text-muted mt-3 ">
+             ${product.description}
+            </p>
+
+            <div class="icons  d-flex gap-2 ">
+
+  <span class="icon  p-2   "  >
+<i class="fa-regular fa-heart "></i>
+   </span>
+<span class="icon  p-2   ">
+    <i class="fa fa-shopping-bag"></i>
+</span>
+
+  <span class="icon  p-2   ">
+        <i class="fa fa-eye"></i>
+    </span> 
+</div>
+
+</div> <!--   paragraph   -->
+  
+
+</div> 
+</div>
+</div>
+                `;
 
                 $("#divlayout1 ").append(html);
-                $("#divlayout2 ").append(html);
+                $("#divlayout2 ").append(html2);
             });
 
         },
