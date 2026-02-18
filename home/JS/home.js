f18e0c4ed53c713003a20d0171db452a0a8c7fe9
YamenAly <yamen.halim@gmail.com>
2026-02-18 21:01:04 +0200

    cart event listener added

--- home/JS/home.js
+++ home/JS/home.js
@@ -2,4 +2,24 @@ $(function(){
     $(".shopnow").click(function(){
         window.location.href = "/productList/Template/product_list.html"
     });
+
+    
 });
+// --- Event Listener 1: Add to Cart ---
+document.addEventListener("DOMContentLoaded", function () {
+    const addToCartButtons = document.querySelectorAll(".btn-warning");
+
+    addToCartButtons.forEach(function (button) {
+        // We check the text to make sure we hit the right button
+        if (button.textContent.trim().toLowerCase() === "add to cart") {
+            button.addEventListener("click", function() {
+                redirectToCart();
+            });
+        }
+    });
+});
+
+function redirectToCart() {
+    console.log("Navigating to Cart...");
+    window.location.href = "cart/Template/cart.html";
+}
