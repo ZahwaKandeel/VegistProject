b137e786b302b540901a192d7321e4a24faf16ac
YamenAly <yamen.halim@gmail.com>
2026-02-20 14:55:33 +0200

    Add & edit forms files added, dummy products file created. Buttons are yet to be connected to the forms

--- component/Product.js
+++ component/Product.js
@@ -9,81 +9,91 @@ export class Product{
         this._imageUrl = imageUrl;
     }
 
-    set ID(_id)
+    set ID(id)
     {
-        if(_id>0)
-        this._id = _id;
+        if(id>0)
+        this._id = id;
     }
     get ID()
     {
-        return this.id
+        return this._id
     }
     
-    set Name(_name)
+    set Name(name)
     {
-
-        this.name = _name;
+        if(name.length<3) throw new Error("Name must be at least 3 characters");
+        this._name = name;
     }
     get Name()
     {
-        return this.name;
+        return this._name;
     }
 
-    set Price(_price)
+    set Price(price)
     {
-        if(_price < 0) throw new Error("Rice cannot be negative");
-        this.price = _price;
+        if(price <= 0) throw new Error("Price cannot be 0 or negative");
+        this._price = price;
     }
 
     get Price()
     {
-        return this.price;
+        return this._price;
     }
     
-    set Description(_description)
+    set Description(description)
     {
-        this.description = _description;
+        if(description.length<100)
+            throw new Error("Description must be at least 100 characters")
+        this._description = description;
     }
     get Description()
     {
-        return this.description;
+        return this._description;
     }
 
-    set Stock(_stock)
+    set Stock(stock)
     {
-        if(_stock < 0) throw new Error("Stock cannot be negative");
-        this.stock = _stock;
+        if(stock < 0) throw new Error("Stock cannot be negative");
+        this._stock = stock;
     }
     get Stock()
     {
-        return this.stock;
+        return this._stock;
     }
 
-    set Category(_category)
+    set Category(category)
     {
-        this.category = _category;
+        const allowed = ["bagel", "candy", "beans", "bestseller", "bread",
+            "biscuite", "breakfast", "cake", "cookie",
+            "cupcake", "Diary&cheese", "Dinner"];
+        if(!allowed.includes(category))
+            throw new Error("Invalid Category");
+        this._category = category;
     }
     get Category()
     {
-        return this.category;
+        return this._category;
     }
 
-    set ImageUrl(_imageUrl)
+    set ImageUrl(imageUrl)
     {
-        this.imageUrl = _imageUrl;
+        const urlPattern = /^(https?:\/\/.*\.(?:png|jpg|jpeg|webp))$/i;
+        if(!urlPattern.test(url))
+            throw new Error("Invalid image URL")
+        this._imageUrl = imageUrl;
     }
     get ImageUrl()
     {
-        return this.imageUrl;
+        return this._imageUrl;
     }
 
    
 }
-function saveProducts(products){
+export function saveProducts(products){
         localStorage.setItem("products", JSON.stringify(products));
     }
 
-function loadProducts(){
+export function loadProducts(){
     let data = localStorage.getItem("products");
     if(!data) return;
     
@@ -97,4 +107,49 @@ function loadProducts(){
         p._category,
         p._imageUrl
     ));
+}
+
+export function editProduct(productID, updatedData){
+    
+    let products = JSON.parse(localStorage.getItem("products")) || [];
+
+    products = products.map(p => new Product(
+        p._id,
+        p._name,
+        p._price,
+        p._description,
+        p._stock,
+        p._category,
+        p._imageUrl,
+    ));
+
+    let product = products.find(p => p.ID === productId);
+
+    if (!product){
+        console.log("Product not found");
+        return false;
+    }
+
+    if(updatedData.name !== undefined)
+        product.Name = updatedData.name;
+
+    if(updatedData.price !== undefined)
+        product.Price = updatedData.price;
+    
+    if(updatedData.description !== undefined)
+        product.Description = updatedData.description;
+    
+    if(updatedData.stock !== undefined)
+        product.Stock = updatedData.stock;
+    
+    if(updatedData.category !== undefined)
+        product.Category = updatedData.category;
+    
+    if(updatedData.imageUrl !== undefined)
+        product.ImageUrl = updatedData.imageUrl;
+
+    localStorage.setItem("products", JSON.stringify(products));
+
+    return true;
+    
 }
