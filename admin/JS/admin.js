b19ab27331b1dcfb3f60a1be47515c62613ce406
Sami <ahmedsamy121277@gmail.com>
2026-02-20 02:44:35 +0200

    new btn add at page product list to seller

--- admin/JS/admin.js
+++ admin/JS/admin.js
@@ -12,7 +12,7 @@ new Chart(ctx, {
   data: {
     labels: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
     datasets: [{
-      data: [120000,350000,450000,120000,200000,180000,300000,120000,250000,350000,250000,180000],
+      data: [120000,350000,450000,120000,200000,180000,200000,120000,250000,350000,250000,180000],
       backgroundColor: gradient,
       borderRadius: 10,
       borderSkipped: false,
@@ -49,3 +49,4 @@ new Chart(ctx, {
     }
   }
 });
+
