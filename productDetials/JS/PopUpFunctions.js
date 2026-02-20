41379f0efd31b313d11585347a2ee20a1aaf769f
ZahwaKandeel <zahwa.bm.kandeel@gmail.com>
2026-02-20 21:55:38 +0200

    PopUp js file for all the js popup needs

---
+++ productDetials/JS/PopUpFunctions.js
@@ -0,0 +1,63 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("quickViewModal.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("modalContainer").innerHTML = data;
+
+            const carouselElement = document.getElementById("carouselPopup");
+
+            if (carouselElement) {
+                new bootstrap.Carousel(carouselElement, {
+                    interval: false,
+                    wrap: true
+                });
+            }
+        });
+});
+
+    // PLUS BUTTON
+    $(document).on('click', '.qty-plus', function () {
+        
+        let input = $(this).siblings('.qty-input');
+        let value = parseInt(input.val());
+
+        input.val(value + 1);
+    });
+
+
+    // MINUS BUTTON
+    $(document).on('click', '.qty-minus', function () {
+
+        let input = $(this).siblings('.qty-input');
+        let value = parseInt(input.val());
+
+        if (value > 1) {
+            input.val(value - 1);
+        }
+    });
+
+$(document).on('click', '.fa-heart', function () {
+    window.location.href = "../../wishlist/Template/wishlist.html";
+});
+
+$(document).on('click', '.fa-shopping-bag', function () {
+    window.location.href = "../../cart/Template/cart.html";
+});
+
+$(document).ready(function () {
+
+    // Add to Cart
+    $(document).on('click', '.btn-warning', function () {
+        if ($(this).text().trim() === "Add to cart") {
+            window.location.href = "../../cart/Template/cart.html";
+        }
+    });
+
+    // Buy Now
+    $(document).on('click', '.btn-dark', function () {
+        if ($(this).text().trim() === "Buy it now") {
+            window.location.href = "../../checkOut/Template/checkOut.html";
+        }
+    });
+
+});
--- productDetials/JS/ProductDetails.js
+++ productDetials/JS/ProductDetails.js
@@ -1,40 +1,4 @@
 $(document).ready(function () {
-
-    // PLUS BUTTON
-    $(document).on('click', '.qty-plus', function () {
-        
-        let input = $(this).siblings('.qty-input');
-        let value = parseInt(input.val());
-
-        input.val(value + 1);
-    });
-
-
-    // MINUS BUTTON
-    $(document).on('click', '.qty-minus', function () {
-
-        let input = $(this).siblings('.qty-input');
-        let value = parseInt(input.val());
-
-        if (value > 1) {
-            input.val(value - 1);
-        }
-    });
-
-    // Add to Cart
-    $(document).on('click', '.btn-warning', function () {
-        if ($(this).text().trim() === "Add to cart") {
-            window.location.href = "../../cart/Template/cart.html";
-        }
-    });
-
-    // Buy Now
-    $(document).on('click', '.btn-dark', function () {
-        if ($(this).text().trim() === "Buy it now") {
-            window.location.href = "../../checkOut/Template/checkOut.html";
-        }
-    });
-
     // Wishlist Link
     $('.bi-heart').closest('a').on('click', function(e) {
         e.preventDefault();
