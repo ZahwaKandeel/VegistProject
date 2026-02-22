afeb80ccf30a958ea38bc7349607bfbe5c837bc6
ahmedsamv <ahmedsamy121277@gmail.com>
2026-02-22 15:13:54 +0200

    fix buttons pagination

--- productList/JS/productlist.js
+++ productList/JS/productlist.js
@@ -66,6 +66,71 @@ userData.forEach(product => {
 document.getElementById("divlayout1").innerHTML = cards;
 
 
+    // breakkkkk--------------------------------------------------------
+
+let cards2 = "";
+
+userData.forEach(product => {
+  cards2 += `
+<div id="${product._id}" class="  cards2    col-12  my-5  " data-category="${product._category}" data-stock="${product._stock}" data-price="${product._price}"> <!--div of card 1  with icons   -->
+
+<div     class="    row  "  > <!--container card with icons     -->
+
+  
+<div class=" position-relative col-12 col-lg-4  "  style="height: 250px;" >    <!--Image -->
+    <img src=" ${ product._imageUrl}" class=" w-100 main-img   object-fit-cover h-100"> 
+    
+</div>
+
+<div class="  col-12 col-lg-8 ">     <!--   name and price and rationg  -->
+
+
+<p class=""> <a href=""  class="para text-decoration-none  " > ${ product._name}</a></p>  <!--   name and price   -->
+        <p class="fw-bold">€${ product._price}</p>   
+
+        <div class="d-flex align-items-center  "> <!-- review by stars -->
+    <div class="text-warning me-2 rating ">
+      <i class=" fa-regular fa-star"></i>
+      <i class="fa-regular fa-star"></i>
+      <i class="fa-regular fa-star"></i>
+      <i class="fa-regular fa-star"></i>
+     <i class="fa-regular fa-star"></i>
+    </div>
+    <span class="text-muted ratingspa">No reviews</span>
+
+  </div>    <!--   name and price   -->
+  <div> <!--   paragraph   -->
+            <p class="text-muted mt-3 ">
+             ${ product._description}
+            </p>
+
+            <div class="icons  d-flex gap-2 ">
+  <span class="icon  p-2   "  >
+<i class="fa-regular fa-heart "></i>
+   </span>
+<span class="icon  p-2   ">
+    <i class="fa fa-shopping-bag"></i>
+</span>
+  <span class="icon  p-2   ">
+        <i class="fa fa-eye"></i>
+    </span> 
+</div>
+</div> <!--   paragraph   -->
+</div> 
+     </div>   <!--container card with icons     -->
+</div>    <!--div of card 1 with icons -->
+
+ `;
+
+
+
+ 
+});
+
+document.getElementById("divlayout2").innerHTML = cards2;
+
+
+
 
     // breakkkkk--------------------------------------------------------
 
@@ -74,6 +139,24 @@ function filterByCategory(category) {
     $(".cards").hide();
     $(`.cards[data-category="${category}"]`).show();
 }
+
+function filterByCategory2(category) {
+    $(".cards2").hide();
+    $(`.cards2[data-category="${category}"]`).show();
+}
+
+$(".bagel").click(function () {
+    filterByCategory2('bagel');
+$(".categName").text('Bagel')
+  let   num = $('.cards2[data-category="bagel"]:visible').length;
+
+    $(".filternum").text(`(${num})`)
+
+$(".catmemb").text(`Bagel`)
+    
+});
+
+
 
 // filter by Price and category
 // function filterProducts(category, minPrice, maxPrice) {
@@ -272,17 +355,29 @@ $(".categName").text('Dinner')
   // that btn to change layout of divs to make it single  
 $(".btnlay2").click(function(){
 
-    $("#divlayout1").addClass("d-none");
+    $("#divlayoutdefult").addClass("d-none");
 
     $("#divlayout2").removeClass("d-none");
+     $("#paginationnav2").removeClass("d-none");
+    $("#paginationnav").addClass("d-none");
+setupPagination("divlayout2", "cards2", "pagination2", 8);
+
+
+
 })
 // that btn to back to defult layout of divs 
 $(".btnlay1").click(function(){
 
-    $("#divlayout1").removeClass("d-none");
+    $("#divlayoutdefult").removeClass("d-none");
     $("#divlayout2").addClass("d-none");
+
+     $("#paginationnav2").addClass("d-none")
+      $("#paginationnav").removeClass("d-none");
+
+setupPagination("divlayout1", "cards", "pagination1", 16);
 })
 
+setupPagination("divlayout1", "cards", "pagination1", 16);
 
 
 
@@ -293,13 +388,13 @@ $(".btnlay1").click(function(){
   
 // })
 
-// $(".btnbag").click(function(){
-//    open("../../productDetials/Template/ProductPopUp.html" ,' width=800px, height=600px')
-// })
+$(".btnbag").click(function(){
+   open("../../productDetials/Template/ProductPopUp.html" ,' width=800px, height=600px')
+})
 
-// $(".btneye").click(function(){
-//    open("../../productDetials/Template/ProductPopUp.html" )
-// })
+$(".btneye").click(function(){
+   open("../../productDetials/Template/ProductPopUp.html" )
+})
 
 
 
@@ -315,56 +410,56 @@ $(".btnlay1").click(function(){
 
 
 
-    let itemsPerPage = 16; // number of products in page
-    let items = $("#divlayout1 .cards");
-    let totalItems = items.length ;
-    let totalPages = Math.ceil(totalItems / itemsPerPage);
+    // let itemsPerPage = 16; // number of products in page
+    // let items = $("#divlayout1 .cards");
+    // let totalItems = items.length ;
+    // let totalPages = Math.ceil(totalItems / itemsPerPage);
 
 
-    console.log(items.length)
-    function showPage(page) {
+    // console.log(items.length)
+    // function showPage(page) {
 
-        items.hide(); 
+    //     items.hide(); 
 
-        const start = (page - 1) * itemsPerPage;
-        const end = start + itemsPerPage;
+    //     const start = (page - 1) * itemsPerPage;
+    //     const end = start + itemsPerPage;
 
-        items.slice(start, end).show();  
-    }
+    //     items.slice(start, end).show();  
+    // }
 
-    //button for slide
-    function createPagination() {  
+    // //button for slide
+    // function createPagination() {  
 
-        $("#pagination").html("");
+    //     $("#pagination").html("");
 
-        for (let i = 1; i <= totalPages; i++) {
-            $("#pagination").append(`
-                <li class="page-item">
-                    <a class="page-link" href="#">${i}</a>
-                </li>
-            `);
-        }
+    //     for (let i = 1; i <= totalPages; i++) {
+    //         $("#pagination").append(`
+    //             <li class="page-item">
+    //                 <a class="page-link" href="#">${i}</a>
+    //             </li>
+    //         `);
+    //     }
 
-        $("#pagination li:first").addClass("active");
+    //     $("#pagination li:first").addClass("active");
 
-        $(".page-link").click(function (e) {
-            e.preventDefault();
+    //     $(".page-link").click(function (e) {
+    //         e.preventDefault();
 
-            const page = parseInt($(this).text());
+    //         const page = parseInt($(this).text());
 
-            $(".page-item").removeClass("active");
-            $(this).parent().addClass("active");
+    //         $(".page-item").removeClass("active");
+    //         $(this).parent().addClass("active");
 
-            showPage(page);
-        });
-    }
+    //         showPage(page);
+    //     });
+    // }
 
-    showPage(1);
-    createPagination();
+    // showPage(1);
+    // createPagination();
 
         //--------------------------------------------------
     //      let itemsPerPage2 = 8; // number of products in page
-    // let items2 = $("#divlayout2 .cards");
+    // let items2 = $("#divlayout2 .cards2");
     // let totalItems2 = items2.length ;
     // let totalPages2 = Math.ceil(totalItems2 / itemsPerPage2);
 
@@ -415,122 +510,8 @@ $(".btnlay1").click(function(){
 //------------------------------------
 
 
-// console.log(userData.age); 
 
 
-// $.ajax({
-//         url: "https://dummyjson.com/products/category/groceries",
-//         method: "GET",
-//         success: function (response) {
-
-// console.log(response.products[1].thumbnail)
-
-//             // Loop through the products array
-//             response.products.forEach(function (product) {
-
-//                 let html = `
-//                     <div class="cards  col-6  col-md-4  col-lg-3   position-relative ">
-//                         <div class=" h-100">
-//                             <img src="${product.thumbnail}" class="card-img-top" alt="${product.title}">
-                          
-//                             <div class="icons position-absolute  start-50 translate-middle-x d-flex gap-2 ">
-
-//   <span class="icon  p-2   "  >
-// <i class="fa-regular fa-heart "></i>
-//    </span>
-// <span class="icon  p-2   ">
-//     <i class="fa fa-shopping-bag"></i>
-// </span>
-
-//   <span class="icon  p-2   ">
-//         <i class="fa fa-eye"></i>
-//     </span> 
-// </div>
-//                                 <h5 class=" para">${product.title}</h5>
-//                                 <p class=" fw-bold">€${product.price}</p>
-//                             </div>
-//                              <div class="d-flex align-items-center  "> <!-- review by stars -->
-//     <div class="text-warning me-2 rating ">
-//       <i class=" fa-regular fa-star"></i>
-//       <i class="fa-regular fa-star"></i>
-//       <i class="fa-regular fa-star"></i>
-//       <i class="fa-regular fa-star"></i>
-//      <i class="fa-regular fa-star"></i>
-      
-//     </div>
-//     <span class="text-muted ratingspa">No reviews</span>
-//   </div>
-// </div>
-//                         </div>
-//                     </div>
-//                 `;
-//                 let html2 = `
-//                   <div class="cards  col-12  my-5    "> <!--div of card 1  with icons   -->
-
-// <div     class="     row  "  >
-//                     <div class=" position-relative col-12 col-lg-4  "  style="height: 250px;" >    <!--Image -->
-//     <img src="${product.thumbnail}" class=" w-100 main-img   object-fit-cover h-100"> 
-//     <img src="../images//1.2.jpg" class=" w-100  hover-img position-absolute top-0 start-0 h-100  ">
-// </div>
-
-// <div class="  col-12 col-lg-8 ">     <!--   name and price and rationg  -->
-
-
-// <p class=""> <a href=""  class="para text-decoration-none  " >${product.title}</a></p>  <!--   name and price   -->
-//         <p class="fw-bold">${product.price}</p>   
-
-//         <div class="d-flex align-items-center  "> <!-- review by stars -->
-//     <div class="text-warning me-2 rating ">
-//       <i class=" fa-regular fa-star"></i>
-//       <i class="fa-regular fa-star"></i>
-//       <i class="fa-regular fa-star"></i>
-//       <i class="fa-regular fa-star"></i>
-//      <i class="fa-regular fa-star"></i>
-      
-//     </div>
-//     <span class="text-muted ratingspa">No reviews</span>
-
-//   </div>    <!--   name and price   -->
-
-
-
-//   <div> <!--   paragraph   -->
-
-//             <p class="text-muted mt-3 ">
-//              ${product.description}
-//             </p>
-
-//             <div class="icons  d-flex gap-2 ">
-
-//   <span class="icon  p-2   "  >
-// <i class="fa-regular fa-heart "></i>
-//    </span>
-// <span class="icon  p-2   ">
-//     <i class="fa fa-shopping-bag"></i>
-// </span>
-
-//   <span class="icon  p-2   ">
-//         <i class="fa fa-eye"></i>
-//     </span> 
-// </div>
-
-// </div> <!--   paragraph   -->
-  
-
-// </div> 
-// </div>
-// </div>
-//                 `;
-
-//                 $("#divlayout1 ").append(html);
-//                 $("#divlayout2 ").append(html2);
-//             });
-
-//         },
-//         error: function (error) {
-//             console.error("Error fetching products:", error);
-//         }
-//     });
 
     // breakkkkk--------------------------------------------------------
 
@@ -538,7 +519,47 @@ $(".btnlay1").click(function(){
 
 
 
+function setupPagination(containerId, cardClass, paginationId, itemsPerPage) {
 
+    let items = $(`#${containerId} .${cardClass}`);
+    let totalItems = items.length;
+    let totalPages = Math.ceil(totalItems / itemsPerPage);
+
+    function showPage(page) {
+        items.hide();
+        const start = (page - 1) * itemsPerPage;
+        const end = start + itemsPerPage;
+        items.slice(start, end).show();
+    }
+
+    function createPagination() {
+        $(`#${paginationId}`).html("");
+
+        for (let i = 1; i <= totalPages; i++) {
+            $(`#${paginationId}`).append(`
+                <li class="page-item">
+                    <a class="page-link" href="#">${i}</a>
+                </li>
+            `);
+        }
+
+        $(`#${paginationId} li:first`).addClass("active");
+
+        $(`#${paginationId} .page-link`).click(function (e) {
+            e.preventDefault();
+
+            const page = parseInt($(this).text());
+
+            $(`#${paginationId} .page-item`).removeClass("active");
+            $(this).parent().addClass("active");
+
+            showPage(page);
+        });
+    }
+
+    showPage(1);
+    createPagination();
+}
 
 
 
