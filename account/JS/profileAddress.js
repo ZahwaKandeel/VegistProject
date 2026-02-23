869fee17e6a8af897d5665cd6aa157b54cab1857
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2026-02-23 23:36:35 +0200

    finished Profile logic + next  is Authorization (Admin, Seller, Customer)

--- account/JS/profileAddress.js
+++ account/JS/profileAddress.js
@@ -2,21 +2,34 @@
 import { User } from "/models/user.js";
 
 window.onload = () => window.scrollTo(0, 0);
+const userData = localStorage.getItem("currentUser");
+const plainUser  = JSON.parse(userData);
+const user = Object.assign(new User({}), plainUser );
+let editingAddressId = null;
+
+$(function() {
+            $("#addAddressBtn").click(function(e) {
+                e.preventDefault();
+                $("#addressForm").toggleClass("visually-hidden");
+            });
+        });
 
 $(function () {
-    const userData = localStorage.getItem("currentUser");
+    
         if (userData) {
-            const plainUser  = JSON.parse(userData);
-            const user = Object.assign(new User({}), plainUser );
 
             if (user.address && user.address.length > 0){
                 for (let i = 0; i < user.address.length; i++) {
                     const addr = user.address[i];
 
                     $("#addAddressSection").before(
-                        `<div class="d-flex justify-content-between align-items-baseline bg-body-secondary bg-opacity-50 mb-4 addDis">
+                        `<div class="d-flex justify-content-between align-items-baseline bg-body-secondary bg-opacity-50 mb-4 addDis"
+                            data-id="${addr.id}">
                             <div class="ms-3 my-2">
-                                <p class="m-0 fullName"><b>${addr.firstName} ${addr.lastName}</b></p>
+                                <p class="m-0 fullName">
+                                    <b>${addr.firstName} ${addr.lastName}</b>
+                                    ${addr.isDefault ? " (Default)": ""}
+                                    </p>
                                 <p class="m-0">${addr.address}, ${addr.apartment}</p>
                                 <p class="m-0">${addr.country}</p>
                                 <p class="m-0">${addr.city}</p>
@@ -30,12 +43,13 @@ $(function () {
                         </div>`
                     )
                 }
-                updateDefaultAddress();
             }
         }
 
             $("form").on("submit", function (e) {
                 e.preventDefault();
+
+                const isDefaultChecked = $("#default").prop("checked");
 
                 const newAddress = {
                     firstName: $("#firstName").val(),
@@ -52,24 +66,90 @@ $(function () {
                     alert("Please fill required fields");
                     return;
                 }
+                // check for default addresses, remove previous default
+                if (isDefaultChecked) {
+                    clearDefaultAddress();
+                    newAddress.isDefault = true;
+                }
 
-                console.log(newAddress);
-                user.addAddress(newAddress);
+                if (editingAddressId !== null) {
+                    const addr = user.address.find(a => a.id === editingAddressId);
+
+                    if (addr) {
+                        Object.assign(addr, newAddress); // update address
+
+                        // If editing default
+                        if (isDefaultChecked) {
+                            clearDefaultAddress();
+                            addr.isDefault = true;
+                        }
+                    }
+                    editingAddressId = null; // exit edit mode
+                }
+                else {
+                    newAddress.id = Date.now();
+                    user.addAddress(newAddress);
+                }
+                localStorage.setItem("currentUser", JSON.stringify(user));
                 window.location.reload();
             });
-        });
 
-function updateDefaultAddress() {
-    $(".fullName").text(function () {
-        return this.textContent.replace(` <b>(Default)</b>`, "");
+            $("form").on("click", "#cancel", function(e) {
+                e.preventDefault();
+
+                $("#firstName").val("");
+                $("#lastName").val("");
+                $("#address").val("");
+                $("#apartment").val("");
+                $("#city").val("");
+                $("#country").val("");
+                $("#postalCode").val("");
+                $("#phone").val("");
+            })
     });
 
-    $(".addDis").first().find(".fullName").append(` <b>(Default)</b>`)
+function clearDefaultAddress() {
+    user.address.forEach(addr => addr.isDefault = false);
 }
 
 $(function() {
     $(".delete").click(function () {
-        $(this).closest(".addDis").remove();
-        updateDefaultAddress();
+        const card = $(this).closest(".addDis");
+        const addressId = Number(card.data("id"));
+
+        const userData = localStorage.getItem("currentUser");
+        const user = JSON.parse(userData);
+
+        user.address = user.address.filter(addr => addr.id !== addressId);
+
+        localStorage.setItem("currentUser", JSON.stringify(user));
+        card.remove();
+    })
+})
+
+$(function() {
+    
+    $(".edit").on("click", function(e) {
+        e.preventDefault();
+
+        
+        const card = $(this).closest(".addDis");
+        const addressId = Number(card.data("id"));
+        const chosenAddress = user.address.find(addr => addr.id === addressId);
+        if (!chosenAddress) return;
+        
+        $("#default").prop("checked", chosenAddress.isDefault === true);
+        editingAddressId = addressId;
+        
+        $("#addressForm").toggleClass("visually-hidden");
+        $("#firstName").val(chosenAddress.firstName);
+        $("#lastName").val(chosenAddress.lastName);
+        $("#address").val(chosenAddress.address);
+        $("#apartment").val(chosenAddress.apartment);
+        $("#city").val(chosenAddress.city);
+        $("#country").val(chosenAddress.country);
+        $("#postalCode").val(chosenAddress.postalCode);
+        $("#phone").val(chosenAddress.phone);
+
     })
 })
